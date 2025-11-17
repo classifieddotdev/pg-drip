c513c60ca0e93d669e8d563dc55de7a5ebb42743
kush-js <88463300+kush-js@users.noreply.github.com>
2025-11-16 23:15:33 -0500

    Revert "fix patroni permissions in entrypoint"
    This reverts commit 1a4dfe061993dd393a532e2d5d5ac879710c3592.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-DATA_DIR="/var/lib/postgresql/patroni"
-CONSUL_DIR="/var/lib/consul"
-
 # fallback: if no PATRONI_NAME set, use hostname
 if [ -z "$PATRONI_NAME" ]; then
   export PATRONI_NAME="$HOSTNAME"
@@ -17,17 +14,11 @@ fi
 # Render patroni.yml with environment variables
 envsubst < /etc/patroni.yml > /tmp/patroni.yml
 
-# Ensure Patroni data dir exists (required when mounted on parent)
-mkdir -p "$DATA_DIR"
-
 # fix consul permissions
 chown -R postgres:postgres /var/lib/consul || true
 # fix patroni permissions
-if [ "$(stat -c '%U' "$DATA_DIR")" != "postgres" ]; then
-  echo "Fixing ownership for $DATA_DIR"
-  chown -R postgres:postgres "$DATA_DIR"
-fi
-chmod 0700 "$DATA_DIR"
+chown -R postgres:postgres /var/lib/postgresql/patroni || true
+chmod 0700 /var/lib/postgresql/patroni
 
 # convenience 
 export PATRONI_CONFIG_FILE=/etc/patroni.yml
